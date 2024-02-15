1df66001a44059ce68193b88fc548333ac2efca6
Melih Ömer Kamar <melih.o.k@hotmail.com>
2024-02-15 23:53:11 +0300

    Add project files.

---
+++ Reasl_Estate_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reasl_Estate_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
